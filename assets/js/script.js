de299cd7afd55558f32eedaf50c1c3517211c53d
bang <yuudachi31@gmail.com>
2019-04-11 15:06:36 +0800

    package js

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+let $ = require('jquery');
+let setText = () => {
+    $('.text').html('HHHHHHHHh');
+};
+
+/*$(document).ready(function () {
+	// when a hanburger menu is clicked
+	let $ul = $(".nav__list"),
+		 $menu = $(".nav__menu");
+
+	$($menu).click(function () {
+		// toggle menu-click Class
+		$ul.toggleClass("menu-click");
+	}); // end click event handler
+});
+*/
